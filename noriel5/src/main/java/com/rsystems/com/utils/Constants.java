5d79b1667a360ab3167c689840e2c838942d60ae
Marius Stancalie <marius.stancalie@yahoo.com>
2021-04-27 13:34:01 +0300

    1

--- noriel5/src/main/java/com/rsystems/com/utils/Constants.java
+++ noriel5/src/main/java/com/rsystems/com/utils/Constants.java
@@ -4,13 +4,13 @@ public final class Constants {
 	public static final String AZURE_ACCOUNT_NAME = "cub02";
 	public static final String AZURE_ACCOUNT_KEY = "dSwJM3Ys+/TxXDKLcv4jJ57zy5zz+b9H+xamc+tt8vd3gv8Ofo3PWdwuOuMRo9h9sC+sCtMUgweyf6nsdYWY0g==";
 	public static final String AZURE_DOC_CONTAINER = "cubcontainer011618789317300";
-	public static final String USER_RECOM = "/home/user_based_item_recommendation4.csv";
-	public static final String PROD_RECOM = "/home/item_based_recommendation4.csv";
-	public static final String ACTION_PATH = "/home/aaa4.csv";
+//	public static final String USER_RECOM = "/home/user_based_item_recommendation4.csv";
+//	public static final String PROD_RECOM = "/home/item_based_recommendation4.csv";
+//	public static final String ACTION_PATH = "/home/aaa4.csv";
 	
-//	public static final String USER_RECOM = "/Users/marius.stancalie/Downloads/user_based_item_recommendation1.csv";
-//	public static final String PROD_RECOM = "/Users/marius.stancalie/Downloads/item_based_recommendation1.csv";
-//	public static final String ACTION_PATH = "/Users/marius.stancalie/Downloads/aaa1.csv";
+	public static final String USER_RECOM = "/Users/marius.stancalie/Downloads/user_based_item_recommendation1.csv";
+	public static final String PROD_RECOM = "/Users/marius.stancalie/Downloads/item_based_recommendation1.csv";
+	public static final String ACTION_PATH = "/Users/marius.stancalie/Downloads/aaa1.csv";
 	
 	
 }
