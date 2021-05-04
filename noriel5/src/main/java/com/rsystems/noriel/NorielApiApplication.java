c8bde7e8aa69b15b9587398cee1e60941857636d
Marius Stancalie <marius.stancalie@yahoo.com>
2021-05-04 14:37:21 +0300

    11

--- noriel5/src/main/java/com/rsystems/noriel/NorielApiApplication.java
+++ noriel5/src/main/java/com/rsystems/noriel/NorielApiApplication.java
@@ -8,7 +8,7 @@ public class NorielApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NorielApiApplication.class, args);
-        // sdsdsdsds
+        // sdsdsdsds dddd
     }
 
 }
